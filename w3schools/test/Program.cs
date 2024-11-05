56744fb387e1ef75bd5f6f8c1b32b7f6e77cd2e4
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-04 23:59:21 -0300

    :books: docs: add first csharp codes into repository

---
+++ w3schools/test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace test{
+    class Program{
+        static void Main (string[] args){
+            const double PI = 3.14159;
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = PI * radius * radius;
+            Console.WriteLine($"AREA={area}");
+        }
+    }
+}
